144719a262da6f36e50c3469cde410a374f666e6
s4vicky <s4vicky@hotmail.com>
2018-06-23 18:57:18 -0700

    Update application.java

--- test456/src/test456/application.java
+++ test456/src/test456/application.java
@@ -3,6 +3,6 @@ package test456;
 public final class application {
 	public static void main (String args[])
 	{
-		System.out.println("works fine now");
+		System.out.println("works fine now now");
 	}
 }
